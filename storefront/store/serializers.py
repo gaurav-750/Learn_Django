0117d95e9c750a4cf5d49ac65ce9a8071e018202
Gaurav <gauravsomani52750@gmail.com>
2022-12-16 17:12:28 +0530

    serializing relationships, ModelSerializers, Deserialization: POST, PUT, also DELETE products

--- storefront/store/serializers.py
+++ storefront/store/serializers.py
@@ -1,14 +1,20 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, Collection
 
 from decimal import Decimal
 
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(
-        max_digits=6, decimal_places=2, source='unit_price')
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description', 'slug', 'unit_price',
+                  'price_with_tax', 'inventory', 'collection']
 
     # *Custom serializer fields:
     price_with_tax = serializers.SerializerMethodField(
@@ -16,3 +22,15 @@ class ProductSerializer(serializers.Serializer):
 
     def calculate_tax(self, product: Product):
         return round(product.unit_price * Decimal(1.1), 2)
+
+    # *Serializing Objects:
+    # collection = serializers.PrimaryKeyRelatedField(
+    #     queryset=Collection.objects.all()
+    # )
+    # collection = serializers.StringRelatedField()
+    # collection = serializers.HyperlinkedRelatedField(
+    #     queryset=Collection.objects.all(),
+    #     view_name='collection-detail'
+    # )
+
+    # collection = CollectionSerializer(read_only=True)
--- storefront/store/views.py
+++ storefront/store/views.py
@@ -5,25 +5,64 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.renderers import JSONRenderer
 
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Product, Collection
+from .serializers import ProductSerializer, CollectionSerializer
 
 # Create your views here.
 
 
-@api_view()
+@api_view(['GET', 'POST'])
 def product_list(req):
-    queryset = Product.objects.all()
-    serializer = ProductSerializer(queryset, many=True)
-    return Response(serializer.data)
+
+    if req.method == 'GET':
+        queryset = Product.objects.select_related('collection').all()
+        serializer = ProductSerializer(
+            queryset, many=True)
+
+        return Response(serializer.data)
+    elif req.method == 'POST':
+        # This is where DeSerialization happens:
+        serializer = ProductSerializer(data=req.data)
+
+        # Data Validation -> If data is inappropriate, raise exception
+        serializer.is_valid(raise_exception=True)
+
+        # save to database:
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view()
+@api_view(['GET', 'PUT', 'DELETE'])
 def product_detail(req, id):
-
     product = get_object_or_404(Product, pk=id)
 
-    # this serializer will convert our product object to a dictionary
-    # and we get that through serializer.data
-    serializer = ProductSerializer(product)
+    if req.method == 'GET':
+        # this serializer will convert our product object to a dictionary
+        # and we get that through serializer.data
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    elif req.method == 'PUT':
+        # deserialize -> validate -> save
+        serializer = ProductSerializer(product, data=req.data)
+        serializer.is_valid(raise_exception=True)
+
+        serializer.save()
+        return Response(serializer.data)
+    elif req.method == 'DELETE':
+        # print(product.orderitem_set.all())
+        # *check if there are any orderitems associated with this product:
+        if product.orderitem_set.count() > 0:
+            return Response({'error': "Product cannot be deleted because it is associated with an order item."},
+                            status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view()
+def collection_detail(req, pk):
+    queryset = Collection.objects.get(pk=pk)
+    serializer = CollectionSerializer(queryset)
+
     return Response(serializer.data)
