1957e4fdf66caf08ba4f1bba251e7abf5da3cfff
Gaurav <gauravsomani52750@gmail.com>
2022-12-16 10:45:34 +0530

    created serializer for Product, added a custom serializer field

---
+++ storefront/store/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import Product
+
+from decimal import Decimal
+
+
+class ProductSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
+    price = serializers.DecimalField(
+        max_digits=6, decimal_places=2, source='unit_price')
+
+    # *Custom serializer fields:
+    price_with_tax = serializers.SerializerMethodField(
+        method_name='calculate_tax')
+
+    def calculate_tax(self, product: Product):
+        return round(product.unit_price * Decimal(1.1), 2)
--- storefront/store/views.py
+++ storefront/store/views.py
@@ -1,20 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.renderers import JSONRenderer
+
+from .models import Product
+from .serializers import ProductSerializer
+
 # Create your views here.
 
 
 @api_view()
 def product_list(req):
-    print('req:', req)
-    return Response('ok')
+    queryset = Product.objects.all()
+    serializer = ProductSerializer(queryset, many=True)
+    return Response(serializer.data)
 
 
 @api_view()
 def product_detail(req, id):
-    print('req', req, id)
-    return Response({
-        "message": "OK",
-        "id": id,
-    })
+
+    product = get_object_or_404(Product, pk=id)
+
+    # this serializer will convert our product object to a dictionary
+    # and we get that through serializer.data
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
